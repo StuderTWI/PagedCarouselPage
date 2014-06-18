f4f2fc8aa7475dc3d824796e500bf915f5ed0e57
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2014-06-18 16:05:28 +0200

    Renderer

--- src/PagedCarouselPage.iOS/PagedCarouselPageRenderer.cs
+++ src/PagedCarouselPage.iOS/PagedCarouselPageRenderer.cs
@@ -8,17 +8,18 @@ using MonoTouch.UIKit;
 using System.Drawing;
 
 
-[assembly:ExportRendererAttribute(typeof(CxPagedCarouselPage), typeof(CxPagedCarouselPageRenderer))]
+[assembly:ExportRendererAttribute (typeof(CxPagedCarouselPage), typeof(CxPagedCarouselPageRenderer))]
 
 namespace CaveBirdLabs.Forms.Platform.iOS
 {
 	public class CxPagedCarouselPageRenderer : CarouselPageRenderer
 	{
+		UIPageControl _pageControl;
+		UILabel _label;
+
 		public CxPagedCarouselPageRenderer ()
 		{
 		}
-
-		UILabel label;
 
 		protected override void OnElementChanged (VisualElementChangedEventArgs e)
 		{
@@ -27,19 +28,35 @@ namespace CaveBirdLabs.Forms.Platform.iOS
 			var page = e.NewElement as CxPagedCarouselPage;
 			var view = NativeView;
 
-			label = new UILabel (new RectangleF (20, 40, view.Frame.Width - 40, 40));
-			label.AdjustsFontSizeToFitWidth = true;
-			label.TextColor = UIColor.White;
-			label.Text = "my renderer";
-			view.Add (label);
+			//TODO: remove this label once Xamarin has checked the app for support
+			_label = new UILabel (new RectangleF (20, 40, view.Frame.Width - 40, 40));
+			_label.AdjustsFontSizeToFitWidth = true;
+			_label.TextColor = UIColor.White;
+			_label.Text = "My Rendered Is Used";
+			view.Add (_label);
+
+
 
 			page.PropertyChanged += OnPropertyChanged;
 
 		}
 
-		private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+		private void OnPropertyChanged (object sender, PropertyChangedEventArgs e)
 		{
-
+			switch (e.PropertyName) {
+			case CxPagedCarouselPage.IsPagerVisibleProperty.PropertyName:
+				break;
+			case CxPagedCarouselPage.PagerItemColorProperty.PropertyName:
+				break;
+			case CxPagedCarouselPage.SelectedPagerItemColorProperty.PropertyName:
+				break;	
+			case CxPagedCarouselPage.PagerPaddingProperty.PropertyName:
+				break;
+			case CxPagedCarouselPage.PagerXAlignProperty.PropertyName:
+				break;
+			case CxPagedCarouselPage.PagerYAlignProperty.PropertyName:
+				break;
+			}
 		}
 
 		public override void ViewDidLoad ()
