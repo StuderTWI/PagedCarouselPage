4b81dcf30fd4f90181f72e6f2a6a2ab467ec3f91
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2014-06-18 09:21:42 +0200

    Issue for renderers not loading from library
    HACK added

--- sample/PagedCarousePage.Sample.iOS/AppDelegate.cs
+++ sample/PagedCarousePage.Sample.iOS/AppDelegate.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using Xamarin.Forms;
+using CaveBirdLabs.Forms.Platform.iOS;
 
 namespace PagedCarouselPage.Sample.iOS
 {
@@ -17,6 +18,9 @@ namespace PagedCarouselPage.Sample.iOS
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			Forms.Init();
+
+			// HACK: this is needed for the Renderers in the library to load (remove this and the renderer is not loaded)
+			CaveBirdLabs.Forms.Platform.iOS.Renderers.Init ();
 
 			// create a new window instance based on the screen size
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
