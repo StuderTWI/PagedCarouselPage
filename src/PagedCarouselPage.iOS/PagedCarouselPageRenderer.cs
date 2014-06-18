8a44d65970ea57d646dfcd7fc157122e43d99984
Stefan <>
2014-06-18 21:04:14 +0200

    Implementing iOS Renderer

--- src/PagedCarouselPage.iOS/PagedCarouselPageRenderer.cs
+++ src/PagedCarouselPage.iOS/PagedCarouselPageRenderer.cs
@@ -1,69 +1,172 @@
-﻿using System;
-using Xamarin.Forms.Platform.iOS;
+﻿#region Usings
+
+using System;
 using System.ComponentModel;
-using Xamarin.Forms;
+using System.Drawing;
 using CaveBirdLabs.Forms;
 using CaveBirdLabs.Forms.Platform.iOS;
 using MonoTouch.UIKit;
-using System.Drawing;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
+#endregion
 
-[assembly:ExportRendererAttribute (typeof(CxPagedCarouselPage), typeof(CxPagedCarouselPageRenderer))]
+[assembly: ExportRenderer(typeof(CxPagedCarouselPage), typeof(CxPagedCarouselPageRenderer))]
 
 namespace CaveBirdLabs.Forms.Platform.iOS
 {
 	public class CxPagedCarouselPageRenderer : CarouselPageRenderer
 	{
-		UIPageControl _pageControl;
-		UILabel _label;
+		#region Declarations
 
-		public CxPagedCarouselPageRenderer ()
+		private bool _currentPageSetByUser = false;
+		private CxPagedCarouselPage _pagedCarouselPage;
+		private UIPageControl _uiPageControl;
+		private UIView _view;
+
+		#endregion
+
+		#region Constructors
+
+		public CxPagedCarouselPageRenderer() { }
+
+		#endregion
+
+		#region Event Handlers
+
+		private void OnPagedCarouselPagePropertyChanged(object sender, PropertyChangedEventArgs e)
 		{
-		}
-
-		protected override void OnElementChanged (VisualElementChangedEventArgs e)
-		{
-			base.OnElementChanged (e);
-
-			var page = e.NewElement as CxPagedCarouselPage;
-			var view = NativeView;
-
-			//TODO: remove this label once Xamarin has checked the app for support
-			_label = new UILabel (new RectangleF (20, 40, view.Frame.Width - 40, 40));
-			_label.AdjustsFontSizeToFitWidth = true;
-			_label.TextColor = UIColor.White;
-			_label.Text = "My Rendered Is Used";
-			view.Add (_label);
-
-			_pageControl = new UIPageControl ();
-			view.Add (_pageControl);
-
-			page.PropertyChanged += OnPropertyChanged;
-
-		}
-
-		private void OnPropertyChanged (object sender, PropertyChangedEventArgs e)
-		{
-			switch (e.PropertyName) {
-			case "IsPagerVisible":
-				break;
-			case "PagerItemColor":
-				break;
-			case "SelectedPagerItemColor":
-				break;	
-			case "PagerPadding":
-				break;
-			case "PagerXAligny":
-				break;
-			case "PagerYAlign":
-				break;
+			switch (e.PropertyName)
+			{
+				case "IsPagerVisible":
+					_uiPageControl.Hidden = !_pagedCarouselPage.IsPagerVisible;
+					break;
+				case "PagerItemColor":
+					SetPageIndicatorTintColor();
+					break;
+				case "SelectedPagerItemColor":
+					SetCurrentPageIndicatorTintColor();
+					break;
+				case "PagerPadding":
+				case "PagerXAlign":
+				case "PagerYAlign":
+					LayoutPageControl();
+					break;
+				case "CurrentPage":
+					if (!_currentPageSetByUser)
+						_uiPageControl.CurrentPage = SelectedIndex;
+					break;
 			}
 		}
 
-		public override void ViewDidLoad ()
+		private void OnUIPageControlValueChanged(object sender, EventArgs eventArgs)
 		{
-			base.ViewDidLoad ();
+			_currentPageSetByUser = true;
+			_pagedCarouselPage.CurrentPage = _pagedCarouselPage.Children[_uiPageControl.CurrentPage];
+			_currentPageSetByUser = false;
 		}
+
+		#endregion
+
+		#region Public Methods
+
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+			_uiPageControl.Pages = _pagedCarouselPage.Children.Count;
+			_pagedCarouselPage.PagerMinimumWidth = _uiPageControl.SizeForNumberOfPages(_uiPageControl.Pages).Width;
+			LayoutPageControl();
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void LayoutPageControl()
+		{
+			float width = _view.Frame.Width - (float)_pagedCarouselPage.PagerPadding.Left - (float)_pagedCarouselPage.PagerPadding.Right;
+			
+			const float HEIGHT = 36; // default height of the UIPageControl
+
+			float x = 0;
+			switch (_pagedCarouselPage.PagerXAlign)
+			{
+				case TextAlignment.Center:
+					if (_view.Frame.Width != width)
+						x = (float)_pagedCarouselPage.PagerPadding.Left + (width / 2);
+
+					break;
+				case TextAlignment.End:
+					x = _view.Frame.Width - width - (float)_pagedCarouselPage.PagerPadding.Right;
+					break;
+				case TextAlignment.Start:
+					x = (float)_pagedCarouselPage.PagerPadding.Left;
+					break;
+			}
+
+			float y = 0;
+			switch (_pagedCarouselPage.PagerYAlign)
+			{
+				case TextAlignment.Center:
+					// ignore top and bottom padding
+					y = (_view.Frame.Height - HEIGHT) / 2;
+					_uiPageControl.HorizontalAlignment = UIControlContentHorizontalAlignment.Center;
+					break;
+				case TextAlignment.End:
+					y = _view.Frame.Height - (float)_pagedCarouselPage.PagerPadding.Bottom - HEIGHT;
+					_uiPageControl.HorizontalAlignment = UIControlContentHorizontalAlignment.Right;
+					break;
+				case TextAlignment.Start:
+					y = (float)_pagedCarouselPage.PagerPadding.Top;
+					_uiPageControl.HorizontalAlignment = UIControlContentHorizontalAlignment.Left;
+					break;
+			}
+
+			_uiPageControl.Frame = new RectangleF(x, y, width, HEIGHT);
+		}
+
+		private void SetCurrentPageIndicatorTintColor()
+		{
+			_uiPageControl.CurrentPageIndicatorTintColor = _pagedCarouselPage.SelectedPagerItemColor != Color.Default ? _pagedCarouselPage.SelectedPagerItemColor.ToUIColor() : UIColor.White;
+		}
+
+		private void SetPageIndicatorTintColor()
+		{
+			_uiPageControl.PageIndicatorTintColor = _pagedCarouselPage.PagerItemColor != Color.Default ? _pagedCarouselPage.PagerItemColor.ToUIColor() : UIColor.Gray;
+		}
+
+		#endregion
+
+		#region Protected Methods
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (disposing)
+			{
+				_pagedCarouselPage.PropertyChanged -= OnPagedCarouselPagePropertyChanged;
+				_uiPageControl.ValueChanged -= OnUIPageControlValueChanged;
+			}
+		}
+
+		protected override void OnElementChanged(VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged(e);
+
+			_pagedCarouselPage = (CxPagedCarouselPage)e.NewElement;
+			_view = NativeView;
+
+			_uiPageControl = new UIPageControl();
+			SetPageIndicatorTintColor();
+			SetCurrentPageIndicatorTintColor();
+
+			_view.Add(_uiPageControl);
+
+			_pagedCarouselPage.PropertyChanged += OnPagedCarouselPagePropertyChanged;
+			_uiPageControl.ValueChanged += OnUIPageControlValueChanged;
+		}
+
+		#endregion
 
 	}
 }
--- src/PagedCarouselPage/PagedCarouselPage.cs
+++ src/PagedCarouselPage/PagedCarouselPage.cs
@@ -1,69 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Usings
 
 using Xamarin.Forms;
+
+#endregion
 
 namespace CaveBirdLabs.Forms
 {
 	public class CxPagedCarouselPage : CarouselPage
 	{
+		#region Declarations
 
 		public static readonly BindableProperty IsPagerVisibleProperty =
-			BindableProperty.Create<CxPagedCarouselPage,bool> (
+			BindableProperty.Create<CxPagedCarouselPage, bool>(
 				p => p.IsPagerVisible, true);
 
-		public bool IsPagerVisible {
-			get { return (bool)GetValue (IsPagerVisibleProperty); }
-			set { SetValue (IsPagerVisibleProperty, value); }
-		}
-
 		public static readonly BindableProperty PagerItemColorProperty =
-			BindableProperty.Create<CxPagedCarouselPage,Color> (
+			BindableProperty.Create<CxPagedCarouselPage, Color>(
 				p => p.PagerItemColor, Color.Default);
 
-		public Color PagerItemColor {
-			get { return (Color)GetValue (PagerItemColorProperty); }
-			set { SetValue (PagerItemColorProperty, value); }
-		}
-
-		public static readonly BindableProperty SelectedPagerItemColorProperty =
-			BindableProperty.Create<CxPagedCarouselPage,Color> (
-				p => p.SelectedPagerItemColor, Color.Default);
-
-		public Color SelectedPagerItemColor {
-			get { return (Color)GetValue (SelectedPagerItemColorProperty); }
-			set { SetValue (SelectedPagerItemColorProperty, value); }
-		}
+		public static readonly BindableProperty PagerMinimumWidthProperty =
+			BindableProperty.Create<CxPagedCarouselPage, double>(
+				p => p.PagerMinimumWidth, 0);
 
 		public static readonly BindableProperty PagerPaddingProperty =
-			BindableProperty.Create<CxPagedCarouselPage,Thickness> (
-				p => p.PagerPadding, new Thickness (0, 0, 0, 60));
-
-		public Thickness PagerPadding {
-			get { return (Thickness)base.GetValue (PagerPaddingProperty); }
-			set { base.SetValue (PagerPaddingProperty, value); }
-		}
+			BindableProperty.Create<CxPagedCarouselPage, Thickness>(
+				p => p.PagerPadding, new Thickness(0, 0, 0, 30));
 
 		public static readonly BindableProperty PagerXAlignProperty =
-			BindableProperty.Create<CxPagedCarouselPage,TextAlignment> (
+			BindableProperty.Create<CxPagedCarouselPage, TextAlignment>(
 				p => p.PagerXAlign, TextAlignment.Center);
 
-		public TextAlignment PagerXAlign {
-			get { return (TextAlignment)base.GetValue (PagerXAlignProperty); }
-			set { base.SetValue (PagerXAlignProperty, value); }
-		}
-
 		public static readonly BindableProperty PagerYAlignProperty =
-			BindableProperty.Create<CxPagedCarouselPage,TextAlignment> (
+			BindableProperty.Create<CxPagedCarouselPage, TextAlignment>(
 				p => p.PagerYAlign, TextAlignment.End);
 
-		public TextAlignment PagerYAlign {
-			get { return (TextAlignment)base.GetValue (PagerYAlignProperty); }
-			set { base.SetValue (PagerYAlignProperty, value); }
+		public static readonly BindableProperty SelectedPagerItemColorProperty =
+			BindableProperty.Create<CxPagedCarouselPage, Color>(
+				p => p.SelectedPagerItemColor, Color.Default);
+
+		#endregion
+
+		#region Public Properties
+
+		public bool IsPagerVisible
+		{
+			get { return (bool)GetValue(IsPagerVisibleProperty); }
+			set { SetValue(IsPagerVisibleProperty, value); }
 		}
 
+		public Color PagerItemColor
+		{
+			get { return (Color)GetValue(PagerItemColorProperty); }
+			set { SetValue(PagerItemColorProperty, value); }
+		}
+
+		public double PagerMinimumWidth
+		{
+			get { return (double)GetValue(PagerMinimumWidthProperty); }
+			internal set { SetValue(PagerMinimumWidthProperty, value); }
+		}
+
+		public Thickness PagerPadding
+		{
+			get { return (Thickness)base.GetValue(PagerPaddingProperty); }
+			set { base.SetValue(PagerPaddingProperty, value); }
+		}
+
+		public TextAlignment PagerXAlign
+		{
+			get { return (TextAlignment)base.GetValue(PagerXAlignProperty); }
+			set { base.SetValue(PagerXAlignProperty, value); }
+		}
+
+		public TextAlignment PagerYAlign
+		{
+			get { return (TextAlignment)base.GetValue(PagerYAlignProperty); }
+			set { base.SetValue(PagerYAlignProperty, value); }
+		}
+
+		public Color SelectedPagerItemColor
+		{
+			get { return (Color)GetValue(SelectedPagerItemColorProperty); }
+			set { SetValue(SelectedPagerItemColorProperty, value); }
+		}
+
+		#endregion
 	}
 }
-
